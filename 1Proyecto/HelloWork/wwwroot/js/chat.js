4a6aa49701c418958a90cb3cbfe0d10016001f46
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-11-10 00:52:49 -0500

    Primer proyecto de ejemplo de signal r

---
+++ 1Proyecto/HelloWork/wwwroot/js/chat.js
@@ -0,0 +1,21 @@
+﻿// el signalR se debe de instalar y usaremos NPM para instalar el paquete(NODE JS)
+const connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiveMessage", (user, message) => {
+    const msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt")
+    const fecha = new Date().toLocaleTimeString();
+    const mensajeAMostrar = fecha + " <strong>" + user + "</strong>:" + msg;
+    const li = document.createElement("li");
+    li.innerHTML = mensajeAMostrar;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().catch(err => console.error(err.toString()));
+
+document.getElementById("sendButton").addEventListener("click", event => {
+    const user = document.getElementById("userInput").value;
+    const message = document.getElementById("messageInput").value;
+    // Llamando desde C#
+    connection.invoke("SendMessage", user, message).catch(err => console.error(err.toString()))
+    event.preventDefault();
+});
